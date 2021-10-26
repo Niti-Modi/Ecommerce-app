8bc9eabf5868354d9cfcec43ea922cfc7879c257
Niti Modi <nitimodi@microsoft.com>
2021-10-26 16:42:04 +0530

    first commit

---
+++ backend/data.js
@@ -0,0 +1,56 @@
+const data = {
+    products: [
+        {
+            _id: '1',
+            name: 'Sensodyne toothpaste',
+            image: '/images1/p1.jpg',
+            price: 120,
+            countInStock: 10,
+
+        },
+
+        {
+            _id: '2',
+            name: 'Colgate Triple Action',
+            image: '/images1/p2.jpg',
+            price: 100,
+            countInStock: 20,
+
+        },
+
+        {
+
+            _id: '3',
+            name: 'EMOFORM-F',
+            image: '/images1/p3.jpg',
+            price: 90,
+            countInStock: 30,
+
+
+        },
+
+        {
+            _id: '4',
+            name: 'Oral-B',
+            image: '/images1/p4.png',
+            price: 220,
+            countInStock: 30,
+        },
+
+        {
+
+            _id: '5',
+            name: 'Crest Pro-Health',
+            image: '/images1/p5.jpg',
+            price: 120,
+            countInStock: 30,
+
+        },
+           
+              
+
+            
+           
+    ],
+};
+export default data;
---
+++ backend/server.js
@@ -0,0 +1,32 @@
+
+import express from 'express';
+
+//in server side prog we append .js at the end
+import data from './data.js';
+
+const app = express();
+//here we define the first route - this route of backend wil respond server is ready
+
+//here we create a handler req, res and inside the body of handler we send back a msg 
+app.get('/', (req, res) => {
+
+    res.send('server is ready');
+
+});
+
+app.get('/api/products', (req, res) => {
+
+    res.send(data.products);
+});
+
+//to make our server running we call listen method of app, here we need to define a default port after listening to this port
+//it should call the body of this func and print console.
+//so when we click on this link it will print server is ready
+const port = process.env.PORT || 5000;
+//if env port does not exist use 5000
+app.listen(5000, () => {
+
+    console.log(`serve at http://localhost:${port}`);
+});
+
+ 

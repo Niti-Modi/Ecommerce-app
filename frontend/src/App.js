9840590355772ccb1bc54738e268a84c877efe5c
Niti Modi <nitimodi@microsoft.com>
2021-10-28 17:30:36 +0530

    Implemented add to cart using redux

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,36 @@
 import React from 'react';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Link, Route } from 'react-router-dom';
 import CartScreen from './screens/CartScreen';
+import { useSelector } from 'react-redux';
 
 
 
 
 function App() {
+    const cart = useSelector((state) => state.cart);
+    const { cartItems } = cart;
+
     return (
         <BrowserRouter>
 
         <div className="grid-container">
             <header className="row">
                 <div>
-                    <a className="brand" href="/">
+                    <Link className="brand" to="/">
                         GlaxoSmithKline
-                    </a>
+                    </Link>
                 </div>
                 <div>
-                    <a href="/cart">Cart</a>
-                    <a href="/signin">Sign In</a>
+                    <Link to="/cart">Cart
+                    
+                    {cartItems.length >0 && (
+                        <span className="badge">{cartItems.length}</span>
+                    )}
+                    
+                    </Link>
+                    <Link to="/signin">Sign In</Link>
                 </div>
             </header>
                 <main>
@@ -30,7 +40,7 @@ function App() {
                 
                 </main>
             <footer className="row center">All right reserved</footer>
-            </div>
+        </div>
 
         </BrowserRouter> 
     );

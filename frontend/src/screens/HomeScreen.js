b3c3a58e08af8e44db094e1ac282ad146d23f513
Niti Modi <nitimodi@microsoft.com>
2021-10-28 17:29:01 +0530

    implemented add To Cart using redux

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -50,8 +50,7 @@ export default function HomeScreen() {
                         
                         
                         )
-                
-                
+                              
                 
                 
                 }
